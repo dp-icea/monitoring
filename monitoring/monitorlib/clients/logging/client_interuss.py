6b1d1d33569fc8f26e21b52dd749cc62f4bca16d
Fernanda Serra <fernandafmst@decea.mil.br>
2024-08-20 11:42:55 -0300

    Updated logging_f3548 config to match resource pool

--- monitoring/monitorlib/clients/logging/client_interuss.py
+++ monitoring/monitorlib/clients/logging/client_interuss.py
@@ -5,7 +5,6 @@ from monitoring.monitorlib.clients.logging.client import (
 from monitoring.monitorlib.fetch import query_and_describe
 from monitoring.monitorlib.infrastructure import UTMClientSession
 
-#TODO: Implement descriptive messages for value error
 class InterUSSLoggingClient(LoggingClient):
     def __init__(self, session: UTMClientSession):
         super(InterUSSLoggingClient, self).__init__()
@@ -16,24 +15,15 @@ class InterUSSLoggingClient(LoggingClient):
         kwargs = {
             "client": self._session,
             "verb": "get",
-            "url": "http://localhost:9091/log?timestamp_start=0&timestamp_end=1",
+            "url": self._session._prefix_url + "log?timestamp_start=0&timestamp_end=1",
         }
         
         query = query_and_describe(**kwargs)
 
         if query.status_code != 200:
-            raise ValueError("Not 200 OK")
+            raise ValueError("GET /log is not available")
         
-        """ try:
-            resp: api.GetVersionResponse = ImplicitDict.parse(
-                query.response.json, api.GetVersionResponse
-            )
-        except:
-            raise ValueError("TBD") """
-        
-        if query.response.json != "Lorem ipsum dolor sit amet":
-            raise ValueError(
-                "Logs dont match"
-            )
+        if query.response.json != {'content':"Lorem ipsum dolor sit amet"}:
+            raise ValueError( "Logs dont match" )
         
         return GetLogResponse(content=query.response.json)
